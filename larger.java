0175dae790dcf7bff30f76f318f8fabc4ee99e6b
Sowmiyadevi44 <sowmisaran2@gmail.com>
2017-06-12 15:23:44 +0530

    Create larger.java

---
+++ larger.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+class larger
+{
+	public static void main(String args[])
+	{
+		int a,b,c;
+		Scanner scan=new Scanner(System.in);
+		System.out.println("enter the number a");
+		a=scan.nextInt();
+		System.out.println("enter the number b");
+		b=scan.nextInt();
+		System.out.println("enter the number c");
+		c=scan.nextInt();
+		if(a>c&&b>c)
+		{
+			System.out.println("a is greater");
+		}
+		else if(b>a)
+		{
+			System.out.println("b is greater");
+		}
+		else
+		System.out.println("c is greater");
+	}
+}
